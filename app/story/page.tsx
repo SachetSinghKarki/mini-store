d886603153a27739f42ef6e7fcf30cd5156c3228
Sachet Singh Karki <karkisachet51@gmail.com>
2026-08-07 15:43:54 +0545

    03: UI Enhancement

--- app/story/page.tsx
+++ app/story/page.tsx
@@ -1,11 +1,11 @@
-import { Hero } from "@/components/Hero";
+import Story from "@/components/Story";
 import { Loading } from "@/modules/products/components/Loading";
 import { Suspense } from "react";
 
 export default function Page() {
   return (
     <Suspense fallback={<Loading />}>
-      <Hero />
+      <Story />
     </Suspense>
   );
 }
